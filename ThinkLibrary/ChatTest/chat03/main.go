ba2c55a16d8a00099e9ba250f6d39d70ea9e0beb
rain <lcmm5201314@gmail.com>
2021-04-22 10:53:27 +0800

    test point

--- ThinkLibrary/ChatTest/chat03/main.go
+++ ThinkLibrary/ChatTest/chat03/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	if err := ret(); err != nil {
-		panic(err)
-	}
-	fmt.Println("Ok.")
+	//if err := ret(); err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Ok.")
+	retTest()
 }
 
 func retErr() (x int, err error) {
@@ -19,8 +20,25 @@ func retErr() (x int, err error) {
 }
 
 func ret() (err error) {
+	//x , err := retErr()
 	if x, err := retErr(); err != nil {
 		fmt.Println("x-> ", x)
 	}
 	return
+}
+
+func retTest() {
+
+	x, err := retErr()
+	fmt.Println("0 x ->", &x, " err->", &err)
+	if err != nil {
+		x, err := retErr()
+		fmt.Println("1 x ->", &x, " err->", &err)
+	}
+	fmt.Println("2 x ->", &x, " err->", &err)
+	x, err = retErr()
+	fmt.Println("3 x ->", &x, " err->", &err)
+	y, err := retErr()
+	fmt.Println("4 x ->", &y, " err->", &err)
+
 }
