b37c8c53410a99321764182529b803c01db3b0d3
rain <lcmm5201314@gmail.com>
2021-06-07 10:10:53 +0800

    test

--- ThinkLibrary/ChatTest/chat09/main.go
+++ ThinkLibrary/ChatTest/chat09/main.go
@@ -1,8 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 
-}
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	//r := []int{8}
+	r := []int{7, 8} //panic
+	for k, v := range a {
+		for _, vv := range r {
+			if v == vv {
+				a = append(a[:k], a[k+1:]...)
+			}
+		}
+	}
 
-type self struct {
+	fmt.Println("a", a)
+
 }
