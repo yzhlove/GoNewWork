0a2627fb8c59840d3c8342165a7d7cded40ee1ae
rain <lcmm5201314@gmail.com>
2021-06-10 14:13:13 +0800

    upd

--- ThinkLibrary/ProtoBuffers/chat05/main.go
+++ ThinkLibrary/ProtoBuffers/chat05/main.go
@@ -1,11 +1,15 @@
 package main
 
+import "log"
+
 func main() {
 
 	Init("127.0.0.1:6379", 1)
 
-	user := &User{Name: "yzh", Age: 0, Birthday: ""}
+	//user := &User{Name: "yzh", Age: 0, Birthday: ""}
+	user := &User{Name: "yzh", Age: 22, Birthday: "2012-12-28"}
 	data, err := user.MarshalMsg(nil)
+	log.Print("data length => ", len(data))
 	if err != nil {
 		panic(err)
 	}
@@ -14,6 +18,13 @@ func main() {
 	if _, err := redis.Get().Do("SET", "Test_Msgp_Key", data); err != nil {
 		panic(err)
 	}
+
+	redis.Get().Do("HMSET", "Test_MSGP_HASH", "KEY_1", data, "KEY_2", data)
+
+	user = &User{Name: "yzh", Age: 0, Birthday: ""}
+	data2, _ := user.MarshalMsg(nil)
+	log.Print("data2 length => ", len(data2))
+	redis.Get().Do("HMSET", "Test_MSGP_HASH", "KEY_1", data, "KEY_2", data2)
 
 	/*
 		user := &User{Name: "yzh", Age: 22, Birthday: "2012-12-28"}
