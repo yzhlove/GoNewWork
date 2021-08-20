f3c03b33593014f3051cdba70df05231059174d5
rain <lcmm5201314@gmail.com>
2021-08-20 10:11:28 +0800

    sha256

---
+++ ThinkLibrary/ChatTest/chat14/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//压缩和解压缩
+
+func main() {
+
+	fmt.Println("res:",0%100)
+
+}
+
+func zipEncode(dst, src string) error {
+
+
+
+	return nil
+}
+
+func zipDecode(dst, src string) error {
+
+	return nil
+}
