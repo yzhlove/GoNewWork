71c7e0e5a5a4be34240781db44229a9ab43ebc30
rain <lcmm5201314@gmail.com>
2021-08-20 16:18:55 +0800

    zip

--- ThinkLibrary/ChatTest/chat17/main.go
+++ ThinkLibrary/ChatTest/chat17/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
+	"crypto/sha256"
+	"fmt"
 	"io"
 	"io/fs"
 	"os"
@@ -10,7 +13,7 @@ import (
 )
 
 func main() {
-	if err := encode("RedisTestChat.zip", "/Users/yostar/workSpace/GoNewWork/RedisTestChat");err != nil {
+	if err := encode("thinkLibrary.zip", "/Users/yostar/workSpace/GoNewWork/ThinkLibrary"); err != nil {
 		panic(err)
 	}
 }
@@ -21,6 +24,8 @@ func encode(dst, src string) error {
 		return err
 	}
 	zw := zip.NewWriter(f)
+	buf := new(bytes.Buffer)
+
 	if err = filepath.Walk(src, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -28,7 +33,8 @@ func encode(dst, src string) error {
 		if !info.Mode().IsRegular() {
 			return nil
 		}
-		w, err := zw.Create(obtained(src, path))
+		name := obtained(src, path)
+		w, err := zw.Create(name)
 		if err != nil {
 			return err
 		}
@@ -36,9 +42,19 @@ func encode(dst, src string) error {
 		if err != nil {
 			return err
 		}
+		hash := checkCode(rf)
+		buf.WriteString(fmt.Sprintf("file:%s\tcheckCode:%s\n", name, hash))
 		_, err = io.Copy(w, rf)
 		return err
 	}); err != nil {
+		return err
+	}
+	verify := src + string(os.PathSeparator) + "verify.txt"
+	w, err := zw.Create(obtained(src, verify))
+	if err != nil {
+		return err
+	}
+	if _, err = io.Copy(w, buf); err != nil {
 		return err
 	}
 	return zw.Close()
@@ -48,4 +64,12 @@ func obtained(src, path string) string {
 	str := strings.Split(src, string(os.PathSeparator))
 	prefix := strings.Join(str[:len(str)-1], string(os.PathSeparator)) + string(os.PathSeparator)
 	return strings.Replace(path, prefix, "", 1)
+}
+
+func checkCode(f io.ReadWriteCloser) string {
+	h256 := sha256.New()
+	if _, err := io.Copy(h256, f); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x", h256.Sum(nil))
 }
