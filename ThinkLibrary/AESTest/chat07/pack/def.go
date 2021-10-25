0c8f0fed7be136628758ae855650f6763d163f4f
yzhlove <lcmm5201314@gmail.com>
2021-10-25 20:59:32 +0800

    test

---
+++ ThinkLibrary/AESTest/chat07/pack/def.go
@@ -0,0 +1,29 @@
+package pack
+
+import "encoding/binary"
+
+type MessageInterface interface {
+	GetID() interface{}
+	GetByteOrder() binary.ByteOrder
+	GetData() []byte
+}
+
+type Pack struct {
+	ID   uint16
+	Data []byte
+}
+
+func (p *Pack) GetID() interface{} {
+	return p.ID
+}
+
+func (p *Pack) GetByteOrder() binary.ByteOrder {
+	return binary.LittleEndian
+}
+
+func (p *Pack)
+
+type Unpack struct {
+	ID   uint16
+	Data []byte
+}
--- ThinkLibrary/AESTest/chat07/pack/pack.go
+++ ThinkLibrary/AESTest/chat07/pack/pack.go
@@ -1,15 +1,40 @@
 package pack
 
-type Message interface {
-	HeadSize() int
-	TagSize()
-	Body() []byte
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"unsafe"
+)
+
+type Message struct {
+	conn io.ReadWriter
+	pack MessageInterface
 }
 
-func Encode() {
+func (m *Message) Encode() error {
 
-}
+	if m.conn == nil {
+		return nil
+	}
 
-func Decode() {
+	dataLen := int(unsafe.Sizeof(m.pack.ID())) + len(m.pack.Data())
+	buf := bytes.NewBuffer([]byte{})
 
+	if err := binary.Write(buf, m.pack.ByteOrder(), uint32(dataLen)); err != nil {
+		return err
+	}
+
+	if err := binary.Write(buf, m.pack.ByteOrder(), m.pack.ID()); err != nil {
+		return err
+	}
+
+	if err := binary.Write(buf, m.pack.ByteOrder(), m.pack.Data()); err != nil {
+		return err
+	}
+
+	if _, err := m.conn.Write(buf.Bytes()); err != nil {
+		return err
+	}
+	return nil
 }
