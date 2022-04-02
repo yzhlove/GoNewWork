4682b792a2002ed67ed414412dcff042f241afaa
yurisa <lcmm5201314@gmail.com>
2022-04-02 15:49:33 +0800

    america

--- ThinkLibrary/TimeZone/chat09/main.go
+++ ThinkLibrary/TimeZone/chat09/main.go
@@ -5,15 +5,53 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
+	"think-library/TimeZone/chat09/assest"
+	"time"
 )
+
+var zonesDef = []string{
+	"Asia/Shanghai",                  // 中国上海
+	"Asia/Urumqi",                    // 中国新疆
+	"Asia/Tokyo",                     // 日本东京
+	"Asia/Seoul",                     // 韩国首尔
+	"America/Adak",                   // 美国阿达克
+	"Pacific/Honolulu",               // 太平洋/檀香山
+	"America/Anchorage",              // 美国/安克雷奇
+	"America/Juneau",                 // 美国/朱诺
+	"America/Metlakatla",             // 美国/梅特拉卡特拉
+	"America/Nome",                   // 美国/诺姆
+	"America/Sitka",                  // 美国/锡特卡
+	"America/Yakutat",                // 美国/亚库塔特
+	"America/Los_Angeles",            // 美国/洛杉矶
+	"America/Boise",                  // 美国/博伊西
+	"America/Denver",                 // 美国/丹佛
+	"America/Phoenix",                // 美国/凤凰城
+	"America/Chicago",                // 美国/芝加哥
+	"America/Indiana/Knox",           // 美国/印第安纳/诺克斯
+	"America/Indiana/Tell_City",      // 美国/印第安纳/泰尔城
+	"America/Menominee",              // 美国/梅诺米尼
+	"America/North_Dakota/Beulah",    // 美国/北达科他/比尤拉
+	"America/North_Dakota/Center",    // 美国/北达科他/中心
+	"America/North_Dakota/New_Salem", // 美国/北达科他/新塞勒姆
+	"America/Detroit",                // 美国/底特律
+	"America/Indiana/Indianapolis",   // 美国/印第安纳/印第安纳波利斯
+	"America/Indiana/Marengo",        // 美国/印第安纳/马伦戈
+	"America/Indiana/Petersburg",     // 美国/印第安纳/彼得斯堡
+	"America/Indiana/Vevay",          // 美国/印第安纳/韦韦
+	"America/Indiana/Vincennes",      // 美国/印第安纳/万塞讷
+	"America/Indiana/Winamac",        // 美国/印第安纳/威纳马克
+	"America/Kentucky/Louisville",    // 美国/肯塔基/路易斯维尔
+	"America/Kentucky/Monticello",    // 美国/肯塔基/蒙蒂塞洛
+	"America/New_York",               // 美国/纽约
+}
 
 func main() {
 
-	test()
+	//test()
 
-	//if err := assest.LoadData(); err != nil {
-	//	panic(err)
-	//}
+	if err := assest.LoadData(); err != nil {
+		panic(err)
+	}
 	//
 	//now := time.Now()
 	//
@@ -26,6 +64,16 @@ func main() {
 	//	strtime := now.In(loc).Format(time.RFC3339)
 	//	fmt.Println("time => ", strtime)
 	//}
+
+	now := time.Now()
+	for _, zone := range zonesDef {
+		loc, err := time.LoadLocationFromTZData(zone, assest.Match(zone))
+		if err != nil {
+			panic(fmt.Sprintf("zone %s error: %s", zone, err))
+		}
+
+		fmt.Println("zone ", zone, "\t\t  time ", now.In(loc).Format(time.RFC3339))
+	}
 
 }
 
