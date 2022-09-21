62b17b8c8b6a7b3ba70017cf6e73e847621f2ea2
rain <lcmm5201314@gmail.com>
2022-09-21 10:54:45 +0800

    zip test

---
+++ ThinkLibrary/ZipChat/chat01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"io"
+	"os"
+)
+
+// golang zip 压缩
+
+func main() {
+	buf := compress([]byte("hello world++++++!"))
+	f, err := os.Create("local.zip")
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(f, buf)
+	f.Sync()
+	f.Close()
+}
+
+func compress(data []byte) *bytes.Buffer {
+
+	buf := bytes.NewBuffer([]byte{})
+
+	zw := zip.NewWriter(buf)
+	defer func() {
+		zw.Flush()
+		zw.Close()
+	}()
+
+	// 可以创建临时文件 os.CreateTemp()
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		os.Remove(file.Name())
+	}()
+
+	fi, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fh, err := zip.FileInfoHeader(fi)
+	if err != nil {
+		panic(err)
+	}
+	w, err := zw.CreateHeader(fh)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := io.Copy(w, bytes.NewReader(data)); err != nil {
+		panic(err)
+	}
+
+	return buf
+}
